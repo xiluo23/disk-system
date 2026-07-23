858e387943c002fe087cc3d4bf1da0601f4c14d9
xiluo23 <130372916+xiluo23@users.noreply.github.com>
2026-07-23 22:09:47 +0800

    Delete crypt/build/Desktop_Qt_6_11_1_MSVC2022_64bit_Debug directory

--- crypt/build/Desktop_Qt_6_11_1_MSVC2022_64bit_Debug/.qt/deploy_test2_ae79a63d47.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/code/qt/crypt/build/Desktop_Qt_6_11_1_MSVC2022_64bit_Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/test2-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "D:/code/qt/crypt/build/Desktop_Qt_6_11_1_MSVC2022_64bit_Debug/test2.exe"
-    GENERATE_QT_CONF
-)
